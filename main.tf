9dcfa82c6c8a0227fd82f9f3eece561ea149b874
Kevin Phelps <kevin.phelps@measurabl.com>
2021-01-21 17:53:22 -0800

    Update label and route53 versions

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.15.0"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.22.1"
   enabled    = var.enabled
   namespace  = var.namespace
   name       = var.name
@@ -91,7 +91,7 @@ resource "aws_security_group_rule" "allow_egress" {
 }
 
 module "dns_host_name" {
-  source  = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=tags/0.3.0"
+  source  = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=tags/0.10.0"
   enabled = local.enabled && length(var.dns_zone_id) > 0 ? "true" : "false"
   name    = var.host_name
   zone_id = var.dns_zone_id
